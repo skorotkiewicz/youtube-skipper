39b15a5e1311ee93e09c1ee08e642181630f3310
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2024-07-04 07:28:54 +0200

    fix scss path

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import YouTube from "react-youtube";
-import "./YouTubeSkipper.scss";
+import "./../YouTubeSkipper.scss";
 import { timeToSeconds } from "../helpers";
 
 const Video = () => {
